0d63a846e1987f8b9d63464e3488b854a7424afb
Tjoosten <Topairy@gmail.com>
2020-10-06 10:35:41 +0200

    Comple role controller tests

--- tests/Feature/Roles/Http/Controllers/RoleControllerTest.php
+++ tests/Feature/Roles/Http/Controllers/RoleControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Roles\Http\Controllers;
 
 use App\Domains\Roles\Http\Controllers\RoleController;
 use App\Domains\Roles\Http\Requests\StoreFormRequest;
+use App\Domains\Roles\Http\Requests\UpdateFormRequest;
 use App\Domains\Roles\Models\Permission;
 use App\Domains\Users\Enums\GroupEnum;
 use App\Domains\Users\Models\Role;
@@ -85,5 +86,57 @@ class RoleControllerTest extends TestCase
         $response = $this->actingAs($me)->get(kioskRoute('roles.show', $role));
         $response->assertSuccessful();
         $response->assertViewIs('roles.show');
+    }
+
+    /** @test */
+    public function userCanViewTheEditForm(): void
+    {
+        $me = User::factory()->create(['user_group' => GroupEnum::WEBMASTER]);
+        $role = Role::factory()->create();
+
+        $this->assertActionUsesMiddleware(RoleController::class, 'edit', ['kiosk', 'web']);
+
+        $response = $this->actingAs($me)->get(kioskRoute('roles.edit', $role));
+        $response->assertSuccessful();
+        $response->assertViewIs('roles.edit');
+    }
+
+    /** @test */
+    public function userCanUpdateAnRoleInTheApplication(): void
+    {
+        $me = User::factory()->create(['user_group' => GroupEnum::WEBMASTER]);
+        $permissions = Permission::factory()->count(4)->create();
+
+        $role = Role::factory()->create();
+        $role->syncPermissions([$permissions[0]->name, $permissions[1]->name]);
+
+        $this->assertTrue($role->hasPermissionTo($permissions[0]->name));
+        $this->assertTrue($role->hasPermissionTo($permissions[1]->name));
+
+        $this->assertActionUsesMiddleware(RoleController::class, 'update', ['auth', 'kiosk']);
+        $this->assertActionUsesFormRequest(RoleController::class, 'update', UpdateFormRequest::class);
+
+        $response = $this->actingAs($me)->patch(kioskRoute('roles.update', $role), [
+            'name' => 'new-role-name',
+            'description' => 'new-description',
+            'permission' => [$permissions[2]->name, $permissions[3]->name]
+        ]);
+
+        $response->assertRedirect(kioskRoute('roles.edit', $role));
+    }
+
+    /** @test */
+    public function userCanDeleteAnPermissionRole(): void
+    {
+        $role = Role::factory()->create();
+        $me = User::factory()->create(['user_group' => GroupEnum::WEBMASTER]);
+
+        $response = $this->actingAs($me)->get(kioskRoute('roles.destroy', $role));
+        $response->assertRedirect(kioskRoute('roles.index'));
+        $response->assertSessionHas([
+            'laravel_flash_message.level' => 'success',
+            'laravel_flash_message.class' => 'alert-success',
+            'laravel_flash_message.message' => __('The permission role is successfully deleted in the application.'),
+        ]);
     }
 }
