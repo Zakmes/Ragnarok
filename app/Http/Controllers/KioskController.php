d3cba898f2841307c93d6777ecf71edf826a6d2a
Tim Joosten <Tjoosten@users.noreply.github.com>
2020-10-19 01:28:53 +0200

    Enhance dashboard (#23)

--- app/Http/Controllers/KioskController.php
+++ app/Http/Controllers/KioskController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Domains\Activity\Models\Activity;
-use App\Domains\Users\Models\Role;
+use App\Domains\Activity\Services\ActivityService;
+use App\Domains\Api\Services\TokenService;
+use App\Domains\Users\Services\RoleService;
+use App\Domains\Users\Services\UserService;
 use App\User;
 use Illuminate\Contracts\Support\Renderable;
 
@@ -14,17 +17,46 @@ use Illuminate\Contracts\Support\Renderable;
  */
 class KioskController extends Controller
 {
-    public function __construct()
-    {
+    private ActivityService $activityService;
+    private UserService $userService;
+    private RoleService $roleService;
+    private TokenService $personalAccessTokenService;
+
+    /**
+     * KioskController constructor.
+     *
+     * @param  ActivityService  $activityService            The service layer for the activity log.
+     * @param  UserService      $userService                The service layer for the user management.
+     * @param  RoleService      $roleService                The service layer for the user permission roles.
+     * @param  TokenService     $personalAccessTokenService The service layer for the personal access tokens. (API)
+     * @return void
+     */
+    public function __construct(
+        ActivityService $activityService,
+        UserService $userService,
+        RoleService $roleService,
+        TokenService $personalAccessTokenService
+    ) {
         $this->middleware(['auth', 'kiosk']);
+
+        $this->activityService = $activityService;
+        $this->userService = $userService;
+        $this->roleService = $roleService;
+        $this->personalAccessTokenService = $personalAccessTokenService;
     }
 
+    /**
+     * Method for displaying the kiosk dashboard page.
+     *
+     * @return Renderable
+     */
     public function __invoke(): Renderable
     {
         return view('kiosk', [
-            'activities' => Activity::orderBy('id', 'DESC')->limit(5)->get(),
-            'users' => User::orderBy('id', 'DESC')->limit(5)->get(),
-            'roles' => Role::orderBy('id', 'DESC')->limit(5)->get()
+            'activities' => $this->activityService->getDashBoardInfo(),
+            'users' => $this->userService->getDashboardInfo(),
+            'roles' => $this->roleService->orderBy('id', 'DESC')->limit(5)->get(),
+            'tokens' => $this->personalAccessTokenService->getDashBoardInfo(),
         ]);
     }
 }
--- public/js/app.js
+++ public/js/app.js
@@ -39397,8 +39397,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/timjoosten/Documents/personal-open-source/spooner/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/timjoosten/Documents/personal-open-source/spooner/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/timjoosten/Documents/spooner/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/timjoosten/Documents/spooner/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
